888433dbc1131d4f42d2a9b176437576a62d24eb
mindex <dev@mindex.com>
2024-10-19 08:22:38 -0400

    Updated Applicant Note comment

--- src/main/java/com/mindex/challenge/data/Employee.java
+++ src/main/java/com/mindex/challenge/data/Employee.java
@@ -12,14 +12,14 @@ import com.fasterxml.jackson.annotation.JsonView;
 /**
  * Employee data object
  * 
- * Notes: I added some annotations to better handle returning a summary version
- * of the Employee (without all their direct reports). This would keep return
- * types less bloated, especially for larger organizations. The directReports
- * could probably be extended in a way to limit the depth as a future feature.
- * One more possible update would be to return the list of directReportIds in
- * the Summary version. This would allow the users to make calls to get
- * direct reports. I also added an Id and DocumentReference to allow the
- * directReports to be loaded and read.
+ * Applicant Note: I added some annotations to better handle returning a
+ * summary version of the Employee (without all their direct reports). This
+ * would keep return types less bloated, especially for larger organizations.
+ * The directReports could probably be extended in a way to limit the depth as
+ * a future feature. One more possible update would be to return the list of
+ * directReportIds in the Summary version. This would allow the users to make
+ * calls to get direct reports. I also added an Id and DocumentReference to
+ * allow the directReports to be loaded and read.
  */
 @Document
 public class Employee {
